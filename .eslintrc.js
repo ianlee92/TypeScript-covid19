3eb15f07836aba9297bf351df9236d1ac898186c
ianlee92 <ethian9227@gmail.com>
2021-06-17 17:40:09 +0900

    ESLint, Babel, Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      node: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error', // warn으로 하면 warning만 뜸
+      {
+        singleQuote: true, // 작은 따옴표
+        semi: true, // 세미콜론
+        useTabs: false, // 탭사용
+        tabWidth: 2, // 탭간격
+        printWidth: 80, // 한줄 80자까지
+        bracketSpacing: true, // { foo: bar } 오브젝트 리터럴 내 간격
+        arrowParens: 'avoid', // 화살표함수 관련 avoid는 x => x, always는 (x) => x
+        endOfLine: 'auto', // Delete `␍` eslint (prettier/prettier) 오류해결
+      },
     ],
-    plugins: ['prettier', '@typescript-eslint'],
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-          semi: true,
-          useTabs: false,
-          tabWidth: 2,
-          printWidth: 80,
-          bracketSpacing: true,
-          arrowParens: 'avoid',
-        },
-      ],
-    },
-    parserOptions: {
-      parser: '@typescript-eslint/parser',
-    },
-  };
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
